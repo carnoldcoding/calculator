b9e054e136528b1d8092404b7c7a7cc7998cda3a
carnoldcoding <carnoldcoding@gmail.com>
2022-04-11 13:53:09 -0400

    Number display working. Limited to 10 digits

--- app.js
+++ app.js
@@ -1,4 +1,35 @@
-const buttons = document.querySelectorAll('.button');
-buttons.forEach(button=>{
-    
+const buttons = document.querySelectorAll(".button");
+const currentDisplay = document.querySelector(".current-display");
+
+function Calculator(){
+    this.currentValue=0;
+    this.lastValue=0;
+    this.display = "";
+
+    this.addToDisplay = function(value){
+        this.display += value;
+    }
+    this.add = function(){
+        this.currentValue += this.lastValue;
+    }
+}
+
+const calc = new Calculator();
+const isNumber = function(number){
+    return (number >= 0 && number <= 9 ? true : false);
+}
+
+buttons.forEach((button)=>{
+    button.addEventListener("click", ()=>{
+        button.style.boxShadow="inset 0 3px 5px 1px rgb(65, 65, 65)";
+        
+        if(isNumber(button.textContent) && calc.display.length < 10){
+            calc.addToDisplay(button.textContent);
+            currentDisplay.textContent=calc.display;
+        }
+        
+        setTimeout(()=>{
+            button.style.boxShadow="none";
+        }, 100)
+    })
 })
