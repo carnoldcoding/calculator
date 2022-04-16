a0b7f50f41e119e37942e2524592bf8bc1f213e5
carnoldcoding <carnoldcoding@gmail.com>
2022-04-16 12:18:42 -0400

    Good prototype finished

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 let firstValue = "";
 let secondValue = "";
 let operator = null;
-let equation = null;
+let equation = "0";
 
 const calculate = function(){
-        a = Number(firstValue);
-        b = Number(secondValue);
-        switch(Calculator.operator){
+        a = Number(secondValue);
+        b = Number(firstValue);
+        switch(operator){
             case '+':
                 return a + b;
             case '-':
@@ -18,6 +18,12 @@ const calculate = function(){
         }
 }
 
+const evaluate = function(){
+    firstValue = calculate();
+    secondValue = "";
+    equation = "0";
+}
+
 const appendNumber = function(value){
     if(firstValue.length <= 10){
         firstValue+=value;
@@ -25,10 +31,31 @@ const appendNumber = function(value){
 }
 const storeOperator = function(value){operator=value;}
 const alterEquation = function(){equation = `${firstValue} ${operator}`}
+const shiftValues = function(){
+    secondValue = firstValue;
+    firstValue = "";
+}
+
+const display = function(){
+    topLine.textContent = equation;
+    bottomLine.textContent = firstValue;
+}
+
+const reset = function(){
+    firstValue = "0";
+    secondValue = "";
+    equation = "";
+    bottomLine.textContent="0";
+    topLine.textContent="0";
+}
 
 const numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
 const buttons = document.querySelectorAll(".button");
+const equals = document.querySelector("#equals");
+const clear = document.querySelector("#clear");
+const topLine = document.querySelector(".top");
+const bottomLine = document.querySelector(".bottom");
 
 //Store values
 numbers.forEach((number)=>{
@@ -37,13 +64,18 @@ numbers.forEach((number)=>{
 
 operators.forEach((operator)=>{
     operator.addEventListener("click", ()=> {
-        storeOperator(operator.textContent)
+        storeOperator(operator.textContent);
         alterEquation();
+        shiftValues();
     })
 })
 
+//Evaluate and Clear
+equals.addEventListener("click", evaluate);
+clear.addEventListener("click", reset);
 //Debug
 buttons.forEach((button)=>{
+    button.addEventListener("click", display);
     button.addEventListener("click", ()=> console.log(`
         firstValue: ${firstValue}
         operator: ${operator}
