dee2e6c11d443580f576afa89b4ab9761ce34712
carnoldcoding <carnoldcoding@gmail.com>
2022-04-16 11:56:38 -0400

    First value, and equation working

--- app.js
+++ app.js
@@ -1,99 +1,54 @@
-//Query DOM
-const buttons = document.querySelectorAll(".button");
-const topLineDOM = document.querySelector(".top");
-const bottomLineDOM = document.querySelector(".bottom");
+let firstValue = "";
+let secondValue = "";
+let operator = null;
+let equation = null;
 
-//Create an object literal for the calculator because we won't be using more
-//Than one instance of the calculator.
-
-const Calculator = {
-    //Attributes
-    currentValue: 0,
-    storedValue: 0,
-    result: null,
-    operator: "",
-
-    topLine: "",
-    bottomLine: "",
-
-    //Methods
-    add: ()=>{Calculator.result = Calculator.storedValue + Calculator.currentValue;},
-    subtract: ()=>{Calculator.result = Calculator.storedValue - Calculator.currentValue;},
-    divide: ()=>{Calculator.result = Calculator.storedValue / Calculator.currentValue;},
-    multiply: ()=>{Calculator.result = Calculator.storedValue * Calculator.currentValue;},
-    calculate: ()=>{
-        if(Calculator.operator=="+"){Calculator.add()}
-        else if(Calculator.operator=="-"){Calculator.subtract()}
-        else if(Calculator.operator=="/"){Calculator.divide()}
-        else if(Calculator.operator=="*"){Calculator.multiply()}
-        Calculator.storedValue = Calculator.result;
-        Calculator.currentValue = 0;
-        Calculator.bottomLine = "";
-    },
-
-    isNumber: (value)=>{return(value >= 0 && value <=9 ? true : false)},
-    addToCurrent: (value)=>{
-        if(Calculator.isNumber(value)){
-            if(Calculator.bottomLine.length < 10){
-                Calculator.bottomLine += value;
-                Calculator.currentValue = Number(Calculator.bottomLine);
-            }
-        }        
-    },
-    addToStorage: (value)=>{
-        if(!Calculator.isNumber(value)){
-            if(Calculator.result == null){
-                if(value != "="){
-                    //Store operator and value
-                    Calculator.operator = value;
-                    Calculator.storedValue = Calculator.currentValue;
-        
-                    //Reset Current
-                    Calculator.currentValue = 0;
-                    Calculator.bottomLine = "";
-                }
-                    
-            }else{
-                if(value != "="){
-                    Calculator.operator = value;
-                }
-            }
-
+const calculate = function(){
+        a = Number(firstValue);
+        b = Number(secondValue);
+        switch(Calculator.operator){
+            case '+':
+                return a + b;
+            case '-':
+                return a - b;
+            case '*':
+                return a * b;
+            case '/':
+                return a / b;
         }
-    },
-    clear: ()=>{
-        Calculator.result = 0;
-        Calculator.currentValue = 0;
-        Calculator.storedValue = 0;
-        Calculator.bottomLine = "";
-        Calculator.topLine = "";
-    },
-    display: ()=>{
-        topLineDOM.textContent = Calculator.storedValue;
-        bottomLineDOM.textContent = Calculator.currentValue;
-    }
-
 }
 
-//Add Functionality
-buttons.forEach((button)=>{button.addEventListener("click", ()=>{
-    Calculator.addToCurrent(button.textContent);
-    Calculator.addToStorage(button.textContent);
-    if(button.textContent=="="){
-        Calculator.calculate();
+const appendNumber = function(value){
+    if(firstValue.length <= 10){
+        firstValue+=value;
     }
-    else if(button.textContent=="C"){
-        Calculator.clear();
-    }
-    Calculator.display();
+}
+const storeOperator = function(value){operator=value;}
+const alterEquation = function(){equation = `${firstValue} ${operator}`}
 
-    console.log(`
-        ==Results==
-        Current: ${Calculator.currentValue}
-        Stored: ${Calculator.storedValue}
-        Operator: ${Calculator.operator}
-        Result: ${Calculator.result}
-        TopLine: ${Calculator.topLine}
-        BottomLine: ${Calculator.bottomLine}
-    `)
-})})
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const buttons = document.querySelectorAll(".button");
+
+//Store values
+numbers.forEach((number)=>{
+    number.addEventListener("click", () => appendNumber(number.textContent))
+})
+
+operators.forEach((operator)=>{
+    operator.addEventListener("click", ()=> {
+        storeOperator(operator.textContent)
+        alterEquation();
+    })
+})
+
+//Debug
+buttons.forEach((button)=>{
+    button.addEventListener("click", ()=> console.log(`
+        firstValue: ${firstValue}
+        operator: ${operator}
+        secondValue: ${secondValue}
+        equation: ${equation}
+    `))
+})
+
